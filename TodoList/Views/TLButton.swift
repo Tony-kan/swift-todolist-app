23796cbe751fad372590c7462d3f9bc22235405b
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-01-06 20:32:55 +0200

    build a register screen

---
+++ TodoList/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  TodoList
+//
+//  Created by Tony on 1/6/25.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
